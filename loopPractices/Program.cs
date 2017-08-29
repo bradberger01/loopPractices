007b16e94845487970b4d1dbeed174e4e62e9640
bradberger01 <bradberger@hotmail.com>
2017-08-29 14:56:01 -0400

    Loops and Arrays practice at home

--- loopPractices/Program.cs
+++ loopPractices/Program.cs
@@ -31,14 +31,35 @@ namespace loopPractices
             //foreach (string band in musicians) 
             //Console.WriteLine(band);
 
-            //Create a program that asks the user for there fav food and stores those values in an array
-            // loop through the array and print all of the foods.
+            //Create a program that asks the user for their fav food and stores those values in an array
 
-            
-            
+            //string[] favFoods = new string[3];
+            //Console.WriteLine("What is your favorite food?");
+            //favFoods[0] = Console.ReadLine();
+            //Console.WriteLine("What is another favorite food?");
+            //favFoods[1] = Console.ReadLine();
+            //Console.WriteLine("Give me one more favorite food.");
+            //favFoods[2] = Console.ReadLine();
 
 
-        //Create an array of lucky numbers using a foreach loop print the following: You lucky number is: 4
+
+            //// loop through the array and print all of the foods.
+
+            //Console.WriteLine("Your favoite foods are:");
+            //foreach (string items in favFoods)
+            //    Console.WriteLine(items);
+
+
+
+
+            //Create an array of lucky numbers using a foreach loop print the following: You lucky number is: 4
+            int[] luckyNumbers = {7,13,21,19,36,80};
+
+            foreach (int myNumber in luckyNumbers)
+
+                Console.WriteLine(" Your lucky number is: " + myNumber);
+
+
         }
     }
 }
