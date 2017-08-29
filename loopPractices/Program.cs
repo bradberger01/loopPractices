3239bae04ac70685df350fa84376cb8da45b1119
bradberger01 <bradberger@hotmail.com>
2017-08-28 20:58:05 -0400

    More Practice

--- loopPractices/Program.cs
+++ loopPractices/Program.cs
@@ -10,6 +10,35 @@ namespace loopPractices
     {
         static void Main(string[] args)
         {
+            //foreach loops -iterate through a collection "goes through each array"
+            //string[] months = { "Jan", "Feb", "March", "April", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
+            //foreach(string month in months)
+            //{
+            //    Console.WriteLine(month);
+            //    if(month == "Dec")
+            //    {
+            //        Console.WriteLine("That's the best month");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("That month is okay I guess");
+            //    }
+
+
+
+            //}
+            //string[] musicians = { "U2", "Maroon 5", "Ed Sheeran", "Nas", "Jarryd" };
+            //foreach (string band in musicians) 
+            //Console.WriteLine(band);
+
+            //Create a program that asks the user for there fav food and stores those values in an array
+            // loop through the array and print all of the foods.
+
+            
+            
+
+
+        //Create an array of lucky numbers using a foreach loop print the following: You lucky number is: 4
         }
     }
 }
