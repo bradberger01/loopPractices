e89bc3b4caca32d79583395d078b2e272368bd31
bradberger01 <bradberger@hotmail.com>
2017-08-30 20:58:43 -0400

    Wednesday Loop/Array/Nested Loop practice

--- loopPractices/Program.cs
+++ loopPractices/Program.cs
@@ -157,15 +157,119 @@ namespace loopPractices
             //while (playAgan == "yes");
 
             //USING A BREAK
-            for(int i = 0; i < 10; i++)
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    if(i==5)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //int x = 1;
+            //while(x <= 10)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //int lives = 3;
+            //int magicNumber = 24;
+            //bool isPlaying = true;
+
+            //while (lives > 0 && isPlaying == true)
+            //{
+            //    Console.WriteLine("Guess my lucky number");
+            //    int userGuess = int.Parse(Console.ReadLine());
+            //    if(magicNumber == userGuess)
+            //    {
+            //        Console.WriteLine("You guessed right");
+            //        isPlaying = false;
+            //        
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Sorry try again");
+            //        lives--;
+            //        Console.WriteLine("You have " + lives + " left.");
+            //    }
+            //}
+
+            //Print numbers 1-100; if the number is a multiple of 3 print fizz, if multible of 5 print buzz, if both print fizz buzz
+
+            //int[] oneToHundred = new int[100];
+            //for(int i = 1; i <= 100; i++) 
+            //{
+            //    if(i % 15 == 0)
+            //    {
+            //        Console.WriteLine("Fizz Buzz");
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if(i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //Create an int array with 10(average) elements; find the mean of these elements and output the results to console
+
+            //int[] tenNumbers = { 12, 18, 27, 34, 85, 63, 31, 45, 17, 20 };
+            //int totalNumber = 0;
+
+            //foreach(int eachNumber in tenNumbers)
+            //{
+            //    totalNumber += eachNumber;// += shorthand - will add up 
+            //}
+
+            //Console.WriteLine(totalNumber / tenNumbers.Length); 
+
+            //NESTED LOOPS
+            //interloop is executed first before moving on to the outer loop
+
+            //1234
+            //1234 = 2 rows, 4 columns
+
+            //for (int i = 1; i <= 2; i++)
+            //{
+            //    for (int j = 1; j <= 4; j++)
+            //    {
+            //        Console.Write(j);//columns
+            //    }
+            //    Console.WriteLine();//row
+            //}
+
+            //print a triangle
+            int n = 4;
+            for(int row = 1; row <= n; row++)
             {
-                Console.WriteLine(i);
-                if(i==5)
+                for(int column = 1; column <= row; column++)
                 {
-                    break;
+                    Console.Write(column + " ");
                 }
+                Console.WriteLine();
             }
 
+            //write a multiplication table using nexted loops
+            string[] favMovies = { "Fight Club", "Die Hard", "Blazing Saddles", "Avatar" };
+            bool vowels = true;
+            foreach (string film in favMovies)
+            {
+                if (film.StartsWith('a' || 'e' || 'i' || 'o' || 'u')
+
+
+                  
+            }
+            {
+
+            }
         }
     }
 }
